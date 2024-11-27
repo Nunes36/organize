c62ff678226abf0b32e0ed239af7d506b1d6fa9c
Matheus Ferreira <theuswork@outlook.com>
2024-11-27 11:31:36 -0300

    autenticacao login e adicionando a pagina dashboard

--- app.py
+++ app.py
@@ -2,20 +2,26 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 import pymysql
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user 
+
 
 pymysql.install_as_MySQLdb()
 
 app = Flask(__name__, template_folder='templates', static_url_path = '/static', static_folder = 'static')
 
-app.secret_key = 'organize'
+app.secret_key = 'organize' #chave de acesso
 
-#configuração do Banco de Dados (usando sqlite como exemplo)
+#configuração do Banco de Dados (usando Mysql como exemplo)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:matheus20@localhost/form'
 app.config['SQLALCHEMY_TRACKMODIFICATIONS'] = False #desativar notificações de modificação
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 
 #
-class Usuario(db.Model):
+class Usuario(UserMixin, db.Model):
     __tablename__ = 'users' #nome da tabela Banco de Dados
 
     #definição das colunas
@@ -27,6 +33,11 @@ class Usuario(db.Model):
     
     def __repr__(self):
         return f"<Usuario {self.nome_completo}>"
+    
+#função carregamento do usuário
+@login_manager.user_loader
+def load_user(user_id):
+    return Usuario.query.get(int(user_id))
 
 #rota para a página de cadastro
 @app.route('/cadastro', methods = ['GET'])
@@ -45,18 +56,17 @@ def cadastrar():
     #verifica se senha coecidem
 
         if senha != conSenha:
-            # 
-            return "As senhas não coecidem", 400
+            flash('As senhas não coecidem!', 'error')
+            return redirect(url_for('cadastro'))
         
         #verifica email cadastrado
         usuarioExistente = Usuario.query.filter_by(email = email).first()
         if usuarioExistente:
-            # return "Este email já está cadastrado", 400
              flash('E-mail já cadastrado. Tente novamente com outro e-mail.', 'error')
              return redirect(url_for('cadastro'))
         
         #criptografia a senha 
-        hashed_password = generate_password_hash(senha)
+        hashed_password = bcrypt.generate_password_hash(senha).decode('utf-8')
 
         
         #criar novo usuario
@@ -68,13 +78,43 @@ def cadastrar():
         db.session.add(novo_usuario)
         db.session.commit()
 
-
+        flash('Cadastro realizado com sucesso! Faça o login para continuar. ', 'sucess')
         return redirect(url_for('home')) #página de login
     return render_template('cadastro.html')
 
+#rota para a página de login
+@app.route('/index', methods = ['GET','POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+
+        #verifica se o usuario existe
+        usuario = Usuario.query.filter_by(email = email).first()
+
+        if usuario and bcrypt.check_password_hash(usuario.senha, senha):
+            login_user(usuario)
+            flash('Login bem-sucedido!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('E-mail ou senha incorretos!', 'error')
+    return render_template('index.html')
+
+#página dashboard (após o login)
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', nome = current_user.nome_completo)
+
+#logout
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Você saiu da sua conta', 'info')
+    return redirect(url_for('login'))
 
 @app.route('/', methods = ['GET'])
-
 def home():
     return render_template('index.html') #página login.html ou index.html
 
